48dcb794778ed636bb77e6cedaf8ef4a4bd88446
RamanMadhukar <ramanmadhukar007@gmail.com>
2025-01-01 20:41:51 +0530

    redirect to balanece record

--- src/components/UTR.jsx
+++ src/components/UTR.jsx
@@ -76,7 +76,7 @@ const UTR = () => {
                     setTimeout(() => {
 
                         // navigate('/deposit_records')
-                        window.location.href = `${redirectUrl}/deposit_records`
+                        window.location.href = `${redirectUrl}/balancerecord`
 
                     }, 3000);
                 }
