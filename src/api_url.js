20104160fb29d7b0ff9b046104d24a0c178be086
HARSH TRIPATHI <60288713+hars123h@users.noreply.github.com>
2025-05-12 20:03:38 +0530

    Update api_url.js

--- src/api_url.js
+++ src/api_url.js
@@ -1,8 +1,8 @@
 // const BASE_URL = "https://whale-app-rqcs6.ondigitalocean.app/api";
 // const BASE_URL = 'https://lobster-app-5gysu.ondigitalocean.app/api'
-const BASE_URL = 'https://nrn-wealth.in/api'
+const BASE_URL = 'https://n3rn.shop/api'
 // const BASE_URL = 'http://localhost:8000/api'
-export const redirectUrl = 'https://nrn-wealth.in'
+export const redirectUrl = 'https://n3rn.shop'
 // export const redirectUrl = 'http://localhost:3001'
 
 // const BASE_URL = 'https://plankton-app-lw2hd.ondigitalocean.app/api'
